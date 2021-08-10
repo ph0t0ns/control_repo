e71561eaa3da8176164194d1c7100baa537989db
ph0t0ns <ph0t0ns@yahoo.co.uk>
2021-08-10 15:42:38 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -16,4 +16,8 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }
 }
